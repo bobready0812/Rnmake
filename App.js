c9ec03dac92eddc224a3dc6f0a0c7ff522587281
DaehyeonKim <Daehyeonoff@gmail.com>
2021-11-01 21:23:29 +0900

    20211101

--- App.js
+++ App.js
@@ -54,39 +54,15 @@ const App: () => Node = () => {
      <View  style={styles.body}>
      <ScrollView>
        <View style={styles.items}>
+         <View>
        <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
        </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
+       <View style={styles.textCon}>
+         <Text>페인트</Text>
+         <Text>남양주시 - 25초전</Text> 
+         <Text>20000원</Text>
        </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
        </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-       <View style={styles.items}>
-       <Image style={styles.images} resizeMode="contain" source={require('./assets/images/paint.jpg')}></Image>
-       </View>
-
      </ScrollView>
     </View>
    <View style={styles.footer}>
@@ -122,7 +98,7 @@ footer: {
 },
 items:{
   
-  
+  flexDirection:"row",
   marginTop:2,
   backgroundColor:"white"
 },
@@ -133,6 +109,11 @@ images: {
   height:150,
   width:150,
   borderRadius:10,
+},
+textCon: {
+  
+  justifyContent:"center"
+  
 }
 });
 
